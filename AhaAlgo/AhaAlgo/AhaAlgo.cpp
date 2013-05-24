16bfe0d6da7575821d4c9ee261d7e79542d2c248
Amery <476179064@qq.com>
2013-05-24 22:52:17 +0800

    AhaAlgo

---
+++ AhaAlgo/AhaAlgo/AhaAlgo.cpp
@@ -0,0 +1,16 @@
+// AhaAlgo.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+using namespace std;
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout <<  "hello" <<endl;
+
+	char a;
+	cin >> a;
+	return 0;
+}
+
